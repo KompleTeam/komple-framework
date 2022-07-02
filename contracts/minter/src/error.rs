2ce2c9f363531342ffd6d3c0f71f949408a672b5
AnalogGhost <analoghost@proton.me>
2022-07-02 23:02:49 +0300

    Add update per address limit method

--- contracts/minter/src/error.rs
+++ contracts/minter/src/error.rs
@@ -16,8 +16,11 @@ pub enum ContractError {
     #[error("Invalid start time")]
     InvalidStartTime {},
 
-    #[error("Minting is already started")]
+    #[error("Minting has already started")]
     AlreadyStarted {},
+
+    #[error("Per address limit must be greater than 0")]
+    InvalidPerAddressLimit {},
 
     #[error("Custom Error val: {val:?}")]
     CustomError { val: String },
