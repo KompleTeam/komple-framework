7daaf66ce8006a848d4deec09861069673e163d2
AnalogGhost <analoghost@proton.me>
2022-07-11 13:26:18 +0300

    Change state

--- contracts/ownership/src/state.rs
+++ contracts/ownership/src/state.rs
@@ -3,3 +3,11 @@ use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub admin: Addr,
+}
+pub const CONFIG: Item<Config> = Item::new("config");
+
+pub const MODULE_ADDR: Item<Addr> = Item::new("module_add");
