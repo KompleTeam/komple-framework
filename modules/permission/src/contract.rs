2f5a318f535d8c553b48c1271fa330372bd53963
AnalogGhost <analoghost@proton.me>
2022-07-11 10:05:34 +0300

    Add permission module contract

---
+++ modules/permission/src/contract.rs
@@ -0,0 +1,45 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:minter";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    Ok(Response::new().add_attribute("action", "instantiate"))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!()
+    // match msg {
+    //     ExecuteMsg::Increment {} => try_increment(deps),
+    //     ExecuteMsg::Reset { count } => try_reset(deps, info, count),
+    // }
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    unimplemented!()
+    // match msg {
+    //     QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
+    // }
+}
