55e6009f843e743a846861500ea407c006e820a9
AnalogGhost <analoghost@proton.me>
2022-07-15 17:01:04 +0300

    Remove passcard module

--- packages/types/src/module.rs
+++ packages/types/src/module.rs
@@ -2,16 +2,14 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 pub const MINT_MODULE_INSTANTIATE_REPLY_ID: u64 = 1;
-pub const PASSCARD_MODULE_INSTANTIATE_REPLY_ID: u64 = 2;
-pub const PERMISSION_MODULE_INSTANTIATE_REPLY_ID: u64 = 3;
-pub const SWAP_MODULE_INSTANTIATE_REPLY_ID: u64 = 4;
-pub const MERGE_MODULE_INSTANTIATE_REPLY_ID: u64 = 5;
+pub const PERMISSION_MODULE_INSTANTIATE_REPLY_ID: u64 = 2;
+pub const SWAP_MODULE_INSTANTIATE_REPLY_ID: u64 = 3;
+pub const MERGE_MODULE_INSTANTIATE_REPLY_ID: u64 = 4;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum Modules {
     MintModule,
-    PasscardModule,
     PermissionModule,
     SwapModule,
     MergeModule,
@@ -21,7 +19,6 @@ impl Modules {
     pub fn as_str(&self) -> &str {
         match self {
             Modules::MintModule => "mint",
-            Modules::PasscardModule => "passcard",
             Modules::PermissionModule => "permission",
             Modules::SwapModule => "swap",
             Modules::MergeModule => "merge",
