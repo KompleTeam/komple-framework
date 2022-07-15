47ab5faa4b4d6246b4566d743e6ad0cfd4913f1c
AnalogGhost <analoghost@proton.me>
2022-07-15 23:10:44 +0300

    Update lock response on metadata contract

--- contracts/metadata/src/msg.rs
+++ contracts/metadata/src/msg.rs
@@ -48,3 +48,9 @@ pub struct MetadataResponse {
     pub metadata: Metadata,
     pub attributes: Vec<Trait>,
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct LockResponse {
+    pub locked: bool,
+}
