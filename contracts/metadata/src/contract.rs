0cc3ead409221dfff856a9ab45217e8083ff0890
AnalogGhost <analoghost@proton.me>
2022-07-15 23:03:57 +0300

    Refactor metadata contract

--- contracts/metadata/src/contract.rs
+++ contracts/metadata/src/contract.rs
@@ -1,10 +1,16 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{
+    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Order, Response, StdResult,
+};
 use cw2::set_contract_version;
+use rift_utils::check_admin_privileges;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::msg::{ExecuteMsg, InstantiateMsg, MetadataResponse, QueryMsg};
+use crate::state::{
+    Config, Metadata, Trait, ATTRIBUTES, COLLECTION_ADDR, CONFIG, METADATA, METADATA_LOCK,
+};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:minter";
@@ -19,6 +25,17 @@ pub fn instantiate(
 ) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
+    let admin = deps.api.addr_validate(&msg.admin)?;
+
+    let config = Config {
+        admin,
+        update_lock: false,
+    };
+
+    CONFIG.save(deps.storage, &config)?;
+
+    COLLECTION_ADDR.save(deps.storage, &info.sender)?;
+
     Ok(Response::new().add_attribute("action", "instantiate"))
 }
 
@@ -29,16 +46,226 @@ pub fn execute(
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
-    // match msg {
+    match msg {
+        ExecuteMsg::AddMetadata {
+            token_id,
+            metadata,
+            attributes,
+        } => execute_add_metadata(deps, env, info, token_id, metadata, attributes),
+        ExecuteMsg::UpdateMetadata { token_id, metadata } => {
+            execute_update_metadata(deps, env, info, token_id, metadata)
+        }
+        ExecuteMsg::AddAttribute {
+            token_id,
+            attribute,
+        } => execute_add_attribute(deps, env, info, token_id, attribute),
+        ExecuteMsg::UpdateAttribute {
+            token_id,
+            attribute,
+        } => execute_update_attribute(deps, env, info, token_id, attribute),
+        ExecuteMsg::RemoveAttribute {
+            token_id,
+            trait_type,
+        } => execute_remove_attribute(deps, env, info, token_id, trait_type),
+    }
+}
 
-    // }
+fn execute_add_metadata(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    token_id: String,
+    metadata: Metadata,
+    attribute: Vec<Trait>,
+) -> Result<Response, ContractError> {
+    let collection_addr = COLLECTION_ADDR.load(deps.storage)?;
+    let config = CONFIG.load(deps.storage)?;
+
+    check_admin_privileges(
+        &info.sender,
+        &env.contract.address,
+        &config.admin,
+        Some(collection_addr),
+        None,
+    )?;
+
+    METADATA.save(deps.storage, &token_id, &metadata)?;
+    for attribute in attribute {
+        ATTRIBUTES.save(
+            deps.storage,
+            (&token_id, &attribute.trait_type),
+            &attribute.value,
+        )?;
+    }
+
+    Ok(Response::new().add_attribute("action", "execute_add_metadata"))
+}
+
+fn execute_update_metadata(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    token_id: String,
+    metadata: Metadata,
+) -> Result<Response, ContractError> {
+    let collection_addr = COLLECTION_ADDR.load(deps.storage)?;
+    let config = CONFIG.load(deps.storage)?;
+
+    check_admin_privileges(
+        &info.sender,
+        &env.contract.address,
+        &config.admin,
+        Some(collection_addr),
+        None,
+    )?;
+    check_metadata_lock(&deps, &config, &token_id)?;
+
+    METADATA.save(deps.storage, &token_id, &metadata)?;
+
+    Ok(Response::new().add_attribute("action", "execute_update_metadata"))
+}
+
+fn execute_add_attribute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    token_id: String,
+    attribute: Trait,
+) -> Result<Response, ContractError> {
+    let collection_addr = COLLECTION_ADDR.load(deps.storage)?;
+    let config = CONFIG.load(deps.storage)?;
+
+    check_admin_privileges(
+        &info.sender,
+        &env.contract.address,
+        &config.admin,
+        Some(collection_addr.clone()),
+        None,
+    )?;
+    check_metadata_lock(&deps, &config, &token_id)?;
+
+    let value = ATTRIBUTES.may_load(deps.storage, (&token_id, &attribute.trait_type))?;
+    if value.is_some() {
+        return Err(ContractError::AttributeAlreadyExists {});
+    }
+
+    ATTRIBUTES.save(
+        deps.storage,
+        (&token_id, &attribute.trait_type),
+        &attribute.value,
+    )?;
+
+    Ok(Response::new().add_attribute("action", "execute_add_attribute"))
+}
+
+fn execute_update_attribute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    token_id: String,
+    attribute: Trait,
+) -> Result<Response, ContractError> {
+    let collection_addr = COLLECTION_ADDR.load(deps.storage)?;
+    let config = CONFIG.load(deps.storage)?;
+
+    check_admin_privileges(
+        &info.sender,
+        &env.contract.address,
+        &config.admin,
+        Some(collection_addr.clone()),
+        None,
+    )?;
+    check_metadata_lock(&deps, &config, &token_id)?;
+
+    let value = ATTRIBUTES.may_load(deps.storage, (&token_id, &attribute.trait_type))?;
+    if value.is_none() {
+        return Err(ContractError::AttributeNotFound {});
+    }
+
+    ATTRIBUTES.save(
+        deps.storage,
+        (&token_id, &attribute.trait_type),
+        &attribute.value,
+    )?;
+
+    Ok(Response::new().add_attribute("action", "execute_update_attribute"))
+}
+
+fn execute_remove_attribute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    token_id: String,
+    trait_type: String,
+) -> Result<Response, ContractError> {
+    let collection_addr = COLLECTION_ADDR.load(deps.storage)?;
+    let config = CONFIG.load(deps.storage)?;
+
+    check_admin_privileges(
+        &info.sender,
+        &env.contract.address,
+        &config.admin,
+        Some(collection_addr.clone()),
+        None,
+    )?;
+    check_metadata_lock(&deps, &config, &token_id)?;
+
+    let value = ATTRIBUTES.may_load(deps.storage, (&token_id, &trait_type))?;
+    if value.is_none() {
+        return Err(ContractError::AttributeNotFound {});
+    }
+
+    ATTRIBUTES.remove(deps.storage, (&token_id, &trait_type));
+
+    Ok(Response::new().add_attribute("action", "execute_remove_attribute"))
+}
+
+fn check_metadata_lock(
+    deps: &DepsMut,
+    config: &Config,
+    token_id: &str,
+) -> Result<(), ContractError> {
+    let metadata_lock = METADATA_LOCK.may_load(deps.storage, token_id)?;
+    if config.update_lock || (metadata_lock.is_some() && metadata_lock.unwrap()) {
+        return Err(ContractError::UpdateLocked {});
+    }
+    Ok(())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-    unimplemented!()
-    // match msg {
+    match msg {
+        QueryMsg::Config {} => to_binary(&query_config(deps)?),
+        QueryMsg::Metadata { token_id } => to_binary(&query_metadata(deps, token_id)?),
+        QueryMsg::MetadataLock { token_id } => to_binary(&query_metadata_lock(deps, token_id)?),
+    }
+}
 
-    // }
+fn query_config(deps: Deps) -> StdResult<Config> {
+    let config = CONFIG.load(deps.storage)?;
+    Ok(config)
+}
+
+fn query_metadata(deps: Deps, token_id: String) -> StdResult<MetadataResponse> {
+    let metadata = METADATA.load(deps.storage, &token_id)?;
+    let attributes = ATTRIBUTES
+        .prefix(&token_id)
+        .range(deps.storage, None, None, Order::Ascending)
+        .map(|item| -> Trait {
+            let r#trait = item.unwrap();
+            Trait {
+                trait_type: r#trait.0,
+                value: r#trait.1,
+            }
+        })
+        .collect::<Vec<Trait>>();
+    Ok(MetadataResponse {
+        metadata,
+        attributes,
+    })
+}
+
+fn query_metadata_lock(deps: Deps, token_id: String) -> StdResult<bool> {
+    let is_locked = METADATA_LOCK.load(deps.storage, &token_id)?;
+    Ok(is_locked)
 }
--- contracts/metadata/src/state.rs
+++ contracts/metadata/src/state.rs
@@ -1,5 +1,35 @@
+use cosmwasm_std::Addr;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct Config {
+    pub admin: Addr,
+    pub update_lock: bool,
+}
+pub const CONFIG: Item<Config> = Item::new("config");
+
+pub const METADATA_LOCK: Map<&str, bool> = Map::new("metadata_lock");
+
+pub const COLLECTION_ADDR: Item<Addr> = Item::new("collection_addr");
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct Trait {
+    // pub display_type: Option<String>,
+    pub trait_type: String,
+    pub value: String,
+}
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
+pub struct Metadata {
+    pub image: Option<String>,
+    pub external_url: Option<String>,
+    pub description: Option<String>,
+    pub animation_url: Option<String>,
+    pub youtube_url: Option<String>,
+}
+pub const METADATA: Map<&str, Metadata> = Map::new("metadata");
+
+// token_id - trait_type -> value
+pub const ATTRIBUTES: Map<(&str, &str), String> = Map::new("attributes");
