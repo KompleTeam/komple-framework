eb4851811da4714e324022b3ac2bd9529e7b5cac
findolor <anakisci@gmail.com>
2022-08-04 23:33:25 +0300

    Add passcard state

--- permissions/passcard/src/state.rs
+++ permissions/passcard/src/state.rs
@@ -1,8 +1,8 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, Uint128};
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
@@ -11,3 +11,26 @@ pub struct Config {
     pub controller_addr: Addr,
 }
 pub const CONFIG: Item<Config> = Item::new("config");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct PasscardInfo {
+    pub name: String,
+    pub description: String,
+    pub image: String,
+    pub external_link: Option<String>,
+    pub total_num: Option<u16>,
+}
+pub const PASSCARD_INFO: Item<PasscardInfo> = Item::new("passcard_info");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Passcard {
+    pub id: u16,
+    pub price: Uint128,
+    pub on_sale: bool,
+    pub owner: Addr,
+}
+pub const PASSCARDS: Item<Passcard> = Item::new("passcards");
+
+pub const MINTABLE_PASSCARDS: Map<(u32, u16), bool> = Map::new("available_passcards");
+
+pub const PASSCARD_IDS: Map<u32, u16> = Map::new("passcard_ids");
