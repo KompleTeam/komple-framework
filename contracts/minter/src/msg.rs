0a8f94dde96bb2c3a31d14e0fd4d50b70287a606
findolor <anakisci@gmail.com>
2022-08-04 23:33:25 +0300

    Add config query to minter contract

--- contracts/minter/src/msg.rs
+++ contracts/minter/src/msg.rs
@@ -28,6 +28,7 @@ pub enum ExecuteMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
+    GetConfig {},
     GetTokenAddress {},
 }
 
