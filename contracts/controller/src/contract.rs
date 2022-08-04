19e8fe30a1edf32ed0dd906939a47f2b7be52957
findolor <anakisci@gmail.com>
2022-08-04 23:33:25 +0300

    Add instantiate method

--- contracts/controller/src/contract.rs
+++ contracts/controller/src/contract.rs
@@ -1,14 +1,20 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{
+    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, SubMsg, WasmMsg,
+};
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{Config, ControllerInfo, COLLECTION_ID, CONFIG, CONTROLLER_INFO};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:minter";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const MINT_INSTANTIATE_REPLY_ID: u64 = 1;
+const MAX_DESCRIPTION_LENGTH: u32 = 512;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -19,9 +25,28 @@ pub fn instantiate(
 ) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
+    let config = Config {
+        admin: info.sender.clone(),
+    };
+    CONFIG.save(deps.storage, &config)?;
+
+    if msg.description.len() > MAX_DESCRIPTION_LENGTH as usize {
+        return Err(ContractError::DescriptionTooLong {});
+    }
+
+    let controller_info = ControllerInfo {
+        name: msg.name,
+        description: msg.description,
+        image: msg.image,
+        external_link: msg.external_link,
+    };
+    CONTROLLER_INFO.save(deps.storage, &controller_info)?;
+
+    COLLECTION_ID.save(deps.storage, &0)?;
+
     Ok(Response::new()
-        .add_attribute("method", "instantiate")
-        .add_attribute("owner", info.sender))
+        .add_attribute("action", "instantiate")
+        .add_attribute("admin", info.sender))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -31,10 +56,12 @@ pub fn execute(
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    match msg {}
+    unimplemented!()
+    // match msg {}
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-    match msg {}
+    unimplemented!()
+    // match msg {}
 }
