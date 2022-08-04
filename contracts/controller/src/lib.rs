2d066549b899760237aab723e169307dfc01e66a
findolor <anakisci@gmail.com>
2022-08-04 23:33:26 +0300

    Refactor test structure

--- contracts/controller/src/lib.rs
+++ contracts/controller/src/lib.rs
@@ -2,6 +2,8 @@ pub mod contract;
 mod error;
 pub mod msg;
 pub mod state;
+
+#[cfg(test)]
 pub mod tests;
 
 pub use crate::error::ContractError;
--- contracts/controller/src/tests/contract_tests.rs
+++ contracts/controller/src/tests/contract_tests.rs
@@ -1,213 +1,210 @@
-#[cfg(test)]
-mod tests {
-    use crate::msg::InstantiateMsg;
-    use cosmwasm_std::{Addr, Coin, Empty, Uint128};
-    use cw_multi_test::{App, AppBuilder, Contract, ContractWrapper, Executor};
+use crate::msg::InstantiateMsg;
+use cosmwasm_std::{Addr, Coin, Empty, Uint128};
+use cw_multi_test::{App, AppBuilder, Contract, ContractWrapper, Executor};
 
-    pub fn controller_contract() -> Box<dyn Contract<Empty>> {
-        let contract = ContractWrapper::new(
-            crate::contract::execute,
-            crate::contract::instantiate,
-            crate::contract::query,
-        )
-        .with_reply(crate::contract::reply);
-        Box::new(contract)
-    }
+pub fn controller_contract() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        crate::contract::execute,
+        crate::contract::instantiate,
+        crate::contract::query,
+    )
+    .with_reply(crate::contract::reply);
+    Box::new(contract)
+}
 
-    pub fn mint_module_contract() -> Box<dyn Contract<Empty>> {
-        let contract = ContractWrapper::new(
-            mint_module::contract::execute,
-            mint_module::contract::instantiate,
-            mint_module::contract::query,
-        )
-        .with_reply(mint_module::contract::reply);
-        Box::new(contract)
-    }
+pub fn mint_module_contract() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        mint_module::contract::execute,
+        mint_module::contract::instantiate,
+        mint_module::contract::query,
+    )
+    .with_reply(mint_module::contract::reply);
+    Box::new(contract)
+}
 
-    pub fn permission_module_contract() -> Box<dyn Contract<Empty>> {
-        let contract = ContractWrapper::new(
-            permission_module::contract::execute,
-            permission_module::contract::instantiate,
-            permission_module::contract::query,
-        );
-        Box::new(contract)
-    }
+pub fn permission_module_contract() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        permission_module::contract::execute,
+        permission_module::contract::instantiate,
+        permission_module::contract::query,
+    );
+    Box::new(contract)
+}
 
-    pub fn token_contract() -> Box<dyn Contract<Empty>> {
-        let contract = ContractWrapper::new(
-            token_contract::contract::execute,
-            token_contract::contract::instantiate,
-            token_contract::contract::query,
-        );
-        Box::new(contract)
-    }
+pub fn token_contract() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        token_contract::contract::execute,
+        token_contract::contract::instantiate,
+        token_contract::contract::query,
+    );
+    Box::new(contract)
+}
 
-    const USER: &str = "juno1shfqtuup76mngspx29gcquykjvvlx9na4kymlm";
-    const ADMIN: &str = "juno1qamfln8u5w8d3vlhp5t9mhmylfkgad4jz6t7cv";
-    const NATIVE_DENOM: &str = "denom";
+const USER: &str = "juno1shfqtuup76mngspx29gcquykjvvlx9na4kymlm";
+const ADMIN: &str = "juno1qamfln8u5w8d3vlhp5t9mhmylfkgad4jz6t7cv";
+const NATIVE_DENOM: &str = "denom";
 
-    fn mock_app() -> App {
-        AppBuilder::new().build(|router, _, storage| {
-            router
-                .bank
-                .init_balance(
-                    storage,
-                    &Addr::unchecked(USER),
-                    vec![Coin {
-                        denom: NATIVE_DENOM.to_string(),
-                        amount: Uint128::new(1),
-                    }],
-                )
-                .unwrap();
-        })
-    }
-
-    fn proper_instantiate(app: &mut App) -> Addr {
-        let controller_code_id = app.store_code(controller_contract());
-
-        let msg = InstantiateMsg {
-            name: "Test Controller".to_string(),
-            description: "Test Controller".to_string(),
-            image: "https://image.com".to_string(),
-            external_link: None,
-        };
-        let controller_contract_addr = app
-            .instantiate_contract(
-                controller_code_id,
-                Addr::unchecked(ADMIN),
-                &msg,
-                &[],
-                "test",
-                None,
+fn mock_app() -> App {
+    AppBuilder::new().build(|router, _, storage| {
+        router
+            .bank
+            .init_balance(
+                storage,
+                &Addr::unchecked(USER),
+                vec![Coin {
+                    denom: NATIVE_DENOM.to_string(),
+                    amount: Uint128::new(1),
+                }],
             )
             .unwrap();
+    })
+}
 
-        controller_contract_addr
-    }
+fn proper_instantiate(app: &mut App) -> Addr {
+    let controller_code_id = app.store_code(controller_contract());
 
-    fn setup_mint_module() {}
+    let msg = InstantiateMsg {
+        name: "Test Controller".to_string(),
+        description: "Test Controller".to_string(),
+        image: "https://image.com".to_string(),
+        external_link: None,
+    };
+    let controller_contract_addr = app
+        .instantiate_contract(
+            controller_code_id,
+            Addr::unchecked(ADMIN),
+            &msg,
+            &[],
+            "test",
+            None,
+        )
+        .unwrap();
 
-    mod modules {
+    controller_contract_addr
+}
+
+fn setup_mint_module() {}
+
+mod modules {
+    use super::*;
+
+    mod mint_module_tests {
         use super::*;
 
-        mod mint_module_tests {
-            use super::*;
+        use crate::{
+            msg::{ExecuteMsg, QueryMsg},
+            ContractError,
+        };
 
-            use crate::{
-                msg::{ExecuteMsg, QueryMsg},
-                ContractError,
+        use rift_types::{module::Modules, query::AddressResponse};
+
+        #[test]
+        fn test_init_happy_path() {
+            let mut app = mock_app();
+            let controller_contract_addr = proper_instantiate(&mut app);
+            let mint_module_code_id = app.store_code(mint_module_contract());
+
+            let msg = ExecuteMsg::InitMintModule {
+                code_id: mint_module_code_id,
             };
-
-            use rift_types::{module::Modules, query::AddressResponse};
-
-            #[test]
-            fn test_init_happy_path() {
-                let mut app = mock_app();
-                let controller_contract_addr = proper_instantiate(&mut app);
-                let mint_module_code_id = app.store_code(mint_module_contract());
-
-                let msg = ExecuteMsg::InitMintModule {
-                    code_id: mint_module_code_id,
-                };
-                let _ = app
-                    .execute_contract(
-                        Addr::unchecked(ADMIN),
-                        controller_contract_addr.clone(),
-                        &msg,
-                        &vec![],
-                    )
-                    .unwrap();
-
-                let msg = QueryMsg::ModuleAddress(Modules::MintModule);
-                let res: AddressResponse = app
-                    .wrap()
-                    .query_wasm_smart(controller_contract_addr, &msg)
-                    .unwrap();
-                assert_eq!(res.address, "contract1")
-            }
-
-            #[test]
-            fn test_init_unhappy_path() {
-                let mut app = mock_app();
-                let controller_contract_addr = proper_instantiate(&mut app);
-                let mint_module_code_id = app.store_code(mint_module_contract());
-
-                let msg = ExecuteMsg::InitMintModule {
-                    code_id: mint_module_code_id,
-                };
-                let err = app
-                    .execute_contract(
-                        Addr::unchecked(USER),
-                        controller_contract_addr.clone(),
-                        &msg,
-                        &vec![],
-                    )
-                    .unwrap_err();
-                assert_eq!(
-                    err.source().unwrap().to_string(),
-                    ContractError::Unauthorized {}.to_string()
+            let _ = app
+                .execute_contract(
+                    Addr::unchecked(ADMIN),
+                    controller_contract_addr.clone(),
+                    &msg,
+                    &vec![],
                 )
-            }
+                .unwrap();
+
+            let msg = QueryMsg::ModuleAddress(Modules::MintModule);
+            let res: AddressResponse = app
+                .wrap()
+                .query_wasm_smart(controller_contract_addr, &msg)
+                .unwrap();
+            assert_eq!(res.address, "contract1")
         }
 
-        mod permission_module_tests {
-            use super::*;
+        #[test]
+        fn test_init_unhappy_path() {
+            let mut app = mock_app();
+            let controller_contract_addr = proper_instantiate(&mut app);
+            let mint_module_code_id = app.store_code(mint_module_contract());
 
-            use crate::{
-                msg::{ExecuteMsg, QueryMsg},
-                ContractError,
+            let msg = ExecuteMsg::InitMintModule {
+                code_id: mint_module_code_id,
             };
-
-            use rift_types::{module::Modules, query::AddressResponse};
-
-            #[test]
-            fn test_init_module() {
-                let mut app = mock_app();
-                let controller_contract_addr = proper_instantiate(&mut app);
-                let permission_module_code_id = app.store_code(permission_module_contract());
-
-                let msg = ExecuteMsg::InitPermissionModule {
-                    code_id: permission_module_code_id,
-                };
-                let _ = app
-                    .execute_contract(
-                        Addr::unchecked(ADMIN),
-                        controller_contract_addr.clone(),
-                        &msg,
-                        &vec![],
-                    )
-                    .unwrap();
-
-                let msg = QueryMsg::ModuleAddress(Modules::PermissionModule);
-                let res: AddressResponse = app
-                    .wrap()
-                    .query_wasm_smart(controller_contract_addr, &msg)
-                    .unwrap();
-                assert_eq!(res.address, "contract1")
-            }
-
-            #[test]
-            fn test_init_unhappy_path() {
-                let mut app = mock_app();
-                let controller_contract_addr = proper_instantiate(&mut app);
-                let permission_module_code_id = app.store_code(permission_module_contract());
-
-                let msg = ExecuteMsg::InitPermissionModule {
-                    code_id: permission_module_code_id,
-                };
-                let err = app
-                    .execute_contract(
-                        Addr::unchecked(USER),
-                        controller_contract_addr.clone(),
-                        &msg,
-                        &vec![],
-                    )
-                    .unwrap_err();
-                assert_eq!(
-                    err.source().unwrap().to_string(),
-                    ContractError::Unauthorized {}.to_string()
+            let err = app
+                .execute_contract(
+                    Addr::unchecked(USER),
+                    controller_contract_addr.clone(),
+                    &msg,
+                    &vec![],
                 )
-            }
+                .unwrap_err();
+            assert_eq!(
+                err.source().unwrap().to_string(),
+                ContractError::Unauthorized {}.to_string()
+            )
+        }
+    }
+
+    mod permission_module_tests {
+        use super::*;
+
+        use crate::{
+            msg::{ExecuteMsg, QueryMsg},
+            ContractError,
+        };
+
+        use rift_types::{module::Modules, query::AddressResponse};
+
+        #[test]
+        fn test_init_module() {
+            let mut app = mock_app();
+            let controller_contract_addr = proper_instantiate(&mut app);
+            let permission_module_code_id = app.store_code(permission_module_contract());
+
+            let msg = ExecuteMsg::InitPermissionModule {
+                code_id: permission_module_code_id,
+            };
+            let _ = app
+                .execute_contract(
+                    Addr::unchecked(ADMIN),
+                    controller_contract_addr.clone(),
+                    &msg,
+                    &vec![],
+                )
+                .unwrap();
+
+            let msg = QueryMsg::ModuleAddress(Modules::PermissionModule);
+            let res: AddressResponse = app
+                .wrap()
+                .query_wasm_smart(controller_contract_addr, &msg)
+                .unwrap();
+            assert_eq!(res.address, "contract1")
+        }
+
+        #[test]
+        fn test_init_unhappy_path() {
+            let mut app = mock_app();
+            let controller_contract_addr = proper_instantiate(&mut app);
+            let permission_module_code_id = app.store_code(permission_module_contract());
+
+            let msg = ExecuteMsg::InitPermissionModule {
+                code_id: permission_module_code_id,
+            };
+            let err = app
+                .execute_contract(
+                    Addr::unchecked(USER),
+                    controller_contract_addr.clone(),
+                    &msg,
+                    &vec![],
+                )
+                .unwrap_err();
+            assert_eq!(
+                err.source().unwrap().to_string(),
+                ContractError::Unauthorized {}.to_string()
+            )
         }
     }
 }
