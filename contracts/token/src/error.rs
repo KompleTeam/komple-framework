abf42ee4770f0e23cc7a90ad8ee643cd9a47253a
findolor <anakisci@gmail.com>
2022-08-04 23:33:25 +0300

    Refactor errors file

--- contracts/token/src/error.rs
+++ contracts/token/src/error.rs
@@ -25,20 +25,11 @@ pub enum ContractError {
 
     #[error("Custom Error val: {val:?}")]
     CustomError { val: String },
-
-    #[error("token_id already claimed")]
-    Claimed {},
-
-    #[error("Cannot set approval that is already expired")]
-    Expired {},
 }
 
 impl From<Cw721ContractError> for ContractError {
     fn from(err: Cw721ContractError) -> ContractError {
         match err {
-            Cw721ContractError::Unauthorized {} => ContractError::Unauthorized {},
-            Cw721ContractError::Claimed {} => ContractError::Claimed {},
-            Cw721ContractError::Expired {} => ContractError::Expired {},
             _ => unreachable!("cannot convert {:?} to Cw721ContractError", err),
         }
     }
