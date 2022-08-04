7aa5ca23bb9e138094195ae866c82c29695f516b
findolor <anakisci@gmail.com>
2022-08-04 23:33:25 +0300

    Add controller msgs

--- contracts/controller/src/msg.rs
+++ contracts/controller/src/msg.rs
@@ -1,13 +1,29 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use mint::msg::InstantiateMsg as MintInstantiateMsg;
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub name: String,
+    pub description: String,
+    pub image: String,
+    pub external_link: Option<String>,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    AddCollection {
+        mint_code_id: u64,
+        mint_instantiate_msg: MintInstantiateMsg,
+    },
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    GetConfig {},
+    GetCollection { collection_id: u32 },
+    GetContollerInfo {},
+}
