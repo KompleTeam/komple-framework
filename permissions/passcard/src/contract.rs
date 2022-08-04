1f68e191974cb7d2980dcf06156a6fb06bd67a9e
findolor <anakisci@gmail.com>
2022-08-04 23:33:25 +0300

    Add passcard queries

--- permissions/passcard/src/contract.rs
+++ permissions/passcard/src/contract.rs
@@ -1,11 +1,13 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{
+    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Order, Response, StdResult,
+};
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{Config, CONFIG};
+use crate::msg::{ExecuteMsg, InstantiateMsg, ListPasscardsResponse, QueryMsg};
+use crate::state::{Config, Passcard, CONFIG, PASSCARDS, PASSCARD_INFO};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:passcard";
@@ -47,12 +49,46 @@ pub fn execute(
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-    unimplemented!()
-    // match msg {
-    // }
+    match msg {
+        QueryMsg::ListAvailablePasscards { collection_id } => {
+            to_binary(&query_available_passcards(deps, collection_id)?)
+        }
+        QueryMsg::GetPasscard {
+            collection_id,
+            passcard_id,
+        } => to_binary(&query_passcard(deps, collection_id, passcard_id)?),
+    }
 }
 
-// fn query_count(deps: Deps) -> StdResult<CountResponse> {
-//     let state = STATE.load(deps.storage)?;
-//     Ok(CountResponse { count: state.count })
-// }
+fn query_available_passcards(deps: Deps, collection_id: u32) -> StdResult<ListPasscardsResponse> {
+    let passcard_info = PASSCARD_INFO.load(deps.storage, collection_id)?;
+
+    // TODO: Implement pagination to this query
+    let passcards = PASSCARDS
+        .range(deps.storage, None, None, Order::Ascending)
+        .filter(|item| {
+            if let Ok(((sub_col_id, _), info)) = item {
+                return sub_col_id == &collection_id && info.on_sale;
+            } else {
+                return false;
+            }
+        })
+        .map(|item| {
+            let ((_, _), info) = item.unwrap();
+
+            Ok(info)
+        })
+        .collect::<StdResult<_>>()?;
+
+    let response = ListPasscardsResponse {
+        passcards,
+        total_num: passcard_info.total_num,
+    };
+
+    Ok(response)
+}
+
+fn query_passcard(deps: Deps, collection_id: u32, passcard_id: u16) -> StdResult<Passcard> {
+    let passcard = PASSCARDS.load(deps.storage, (collection_id, passcard_id))?;
+    Ok(passcard)
+}
--- permissions/passcard/src/msg.rs
+++ permissions/passcard/src/msg.rs
@@ -1,6 +1,8 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use crate::state::Passcard;
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
     pub controller_address: String,
@@ -12,4 +14,18 @@ pub enum ExecuteMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    ListAvailablePasscards {
+        collection_id: u32,
+    },
+    GetPasscard {
+        collection_id: u32,
+        passcard_id: u16,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ListPasscardsResponse {
+    pub total_num: Option<u16>,
+    pub passcards: Vec<Passcard>,
+}
