2d18c895170e029dc5bf5971b138a50e348e92b1
findolor <anakisci@gmail.com>
2022-08-04 23:33:25 +0300

    Add mint test

--- contracts/token/src/contract_tests.rs
+++ contracts/token/src/contract_tests.rs
@@ -1,30 +1,71 @@
 #[cfg(test)]
 mod tests {
     use crate::{
-        contract::{execute, instantiate},
-        msg::ExecuteMsg,
+        contract::{execute, instantiate, query},
+        msg::{ExecuteMsg, QueryMsg},
         state::Locks,
         ContractError,
     };
     use cosmwasm_std::{
-        attr,
+        attr, from_binary,
         testing::{mock_dependencies, mock_env, mock_info},
         Empty,
     };
+    use cw721::OwnerOfResponse;
     use cw721_base::{msg::InstantiateMsg, MintMsg};
+
+    #[test]
+    fn mint() {
+        let admin_info = mock_info("admin", &vec![]);
+        let mut deps = mock_dependencies();
+        let msg = InstantiateMsg {
+            name: "Test Project".to_string(),
+            symbol: "TP".to_string(),
+            minter: "admin".to_string(),
+        };
+        let res = instantiate(deps.as_mut(), mock_env(), admin_info.clone(), msg);
+        assert!(res.is_ok());
+
+        let msg = ExecuteMsg::Mint(MintMsg {
+            token_id: "1".to_string(),
+            owner: "admin".to_string(),
+            token_uri: None,
+            extension: Empty {},
+        });
+        let res = execute(deps.as_mut(), mock_env(), admin_info, msg).unwrap();
+        assert_eq!(
+            res.attributes,
+            vec![
+                attr("action", "mint"),
+                attr("minter", "admin"),
+                attr("owner", "admin"),
+                attr("token_id", "1"),
+            ],
+        );
+
+        let res = query(
+            deps.as_ref(),
+            mock_env(),
+            QueryMsg::OwnerOf {
+                token_id: "1".to_string(),
+                include_expired: None,
+            },
+        )
+        .unwrap();
+        let data: OwnerOfResponse = from_binary(&res).unwrap();
+        assert_eq!(data.owner, "admin");
+    }
 
     #[test]
     fn update_locks() {
         let admin_info = mock_info("admin", &vec![]);
         let random_info = mock_info("random", &vec![]);
         let mut deps = mock_dependencies();
-
         let msg = InstantiateMsg {
             name: "Test Project".to_string(),
             symbol: "TP".to_string(),
             minter: "admin".to_string(),
         };
-
         let res = instantiate(deps.as_mut(), mock_env(), admin_info.clone(), msg);
         assert!(res.is_ok());
 
