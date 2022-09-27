8b8870a25d22834d5cb701d2a1a0f12b488f87b0
findolor <anakisci@gmail.com>
2022-09-27 08:00:23 -0500

    Upgrade hub module to cw1.1

--- contracts/hub/src/msg.rs
+++ contracts/hub/src/msg.rs
@@ -1,11 +1,10 @@
+use crate::state::{HubInfo, WebsiteConfig};
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use komple_types::{module::Modules, query::ResponseWrapper};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use komple_types::module::Modules;
-
-use crate::state::{HubInfo, WebsiteConfig};
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[cw_serde]
 pub struct InstantiateMsg {
     pub name: String,
     pub description: String,
@@ -13,8 +12,7 @@ pub struct InstantiateMsg {
     pub external_link: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub enum ExecuteMsg {
     // Instantiates a new mint module
     InitMintModule {
@@ -54,12 +52,15 @@ pub enum ExecuteMsg {
     },
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
+#[derive(QueryResponses)]
 pub enum QueryMsg {
     // Gets both general and website config
+    #[returns(ResponseWrapper<ConfigResponse>)]
     Config {},
+    #[returns(ResponseWrapper<String>)]
     ModuleAddress(Modules),
+    #[returns(ResponseWrapper<Vec<String>>)]
     Operators {},
 }
 
