b93772951607ac5673604d1aca6ca4e979899a62
findolor <anakisci@gmail.com>
2022-10-03 20:22:04 +0300

    Add optimizer script

---
+++ scripts/optimize.sh
@@ -0,0 +1,12 @@
+optimize() {
+  docker run --rm -v "$(pwd)":/code \
+  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/workspace-optimizer-arm64:0.12.8
+}
+
+cd ../modules
+optimize
+
+cd ../permissions
+optimize
