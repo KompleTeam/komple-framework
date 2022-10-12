5939ddf75a2ca9d30b80bfefd3b8a1a7fd1b0a4b
findolor <anakisci@gmail.com>
2022-10-12 21:32:33 +0300

    Refactor permission tests

--- contracts/ownership-permission/src/contract.rs
+++ contracts/ownership-permission/src/contract.rs
@@ -1,8 +1,8 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    from_binary, to_binary, Addr, Attribute, Binary, Deps, DepsMut, Env, MessageInfo,
-    Response, StdResult,
+    from_binary, to_binary, Addr, Attribute, Binary, Deps, DepsMut, Env, MessageInfo, Response,
+    StdResult,
 };
 use cw2::set_contract_version;
 use komple_types::module::Modules;
--- contracts/permission/tests/ownership_permission_integration.rs
+++ contracts/permission/tests/ownership_permission_integration.rs
@@ -1,22 +1,31 @@
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::ContractError;
+use cosmwasm_std::to_binary;
 use cosmwasm_std::{Addr, Coin, Empty, Uint128};
 use cw_multi_test::{App, AppBuilder, Contract, ContractWrapper, Executor};
-use komple_types::query::ResponseWrapper;
+use komple_ownership_permission_module::msg::InstantiateMsg as OwnershipModuleInstantiateMsg;
+use komple_permission_module::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use komple_types::permission::Permissions;
 
 pub fn permission_module() -> Box<dyn Contract<Empty>> {
     let contract = ContractWrapper::new(
-        crate::contract::execute,
-        crate::contract::instantiate,
-        crate::contract::query,
+        komple_permission_module::contract::execute,
+        komple_permission_module::contract::instantiate,
+        komple_permission_module::contract::query,
     )
-    .with_reply(crate::contract::reply);
+    .with_reply(komple_permission_module::contract::reply);
+    Box::new(contract)
+}
+
+pub fn ownership_permission_module() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        komple_ownership_permission_module::contract::execute,
+        komple_ownership_permission_module::contract::instantiate,
+        komple_ownership_permission_module::contract::query,
+    );
     Box::new(contract)
 }
 
 const USER: &str = "juno..user";
 const ADMIN: &str = "juno..admin";
-const RANDOM: &str = "juno..random";
 const NATIVE_DENOM: &str = "native_denom";
 
 fn mock_app() -> App {
@@ -58,54 +67,42 @@ fn proper_instantiate(app: &mut App) -> Addr {
 mod actions {
     use super::*;
 
-    mod update_operators {
+    mod register_permission {
+        use komple_types::query::ResponseWrapper;
+
         use super::*;
 
         #[test]
-        fn test_update_happy_path() {
+        fn test_happy_path() {
             let mut app = mock_app();
             let permission_module_addr = proper_instantiate(&mut app);
 
-            let msg = ExecuteMsg::UpdateOperators {
-                addrs: vec![RANDOM.to_string(), RANDOM.to_string()],
+            let ownership_permission_code_id = app.store_code(ownership_permission_module());
+            let msg = ExecuteMsg::RegisterPermission {
+                permission: Permissions::Ownership.to_string(),
+                msg: to_binary(&OwnershipModuleInstantiateMsg {
+                    admin: ADMIN.to_string(),
+                })
+                .unwrap(),
+                code_id: ownership_permission_code_id,
             };
             let _ = app
                 .execute_contract(
                     Addr::unchecked(ADMIN),
                     permission_module_addr.clone(),
                     &msg,
-                    &vec![],
+                    &[],
                 )
                 .unwrap();
 
-            let msg = QueryMsg::Operators {};
-            let res: ResponseWrapper<Vec<String>> = app
+            let msg = QueryMsg::PermissionAddress {
+                permission: Permissions::Ownership.to_string(),
+            };
+            let res: ResponseWrapper<String> = app
                 .wrap()
                 .query_wasm_smart(permission_module_addr, &msg)
                 .unwrap();
-            assert_eq!(res.data, vec![RANDOM.to_string()]);
-        }
-
-        #[test]
-        fn test_update_unhappy_path() {
-            let mut app = mock_app();
-            let permission_module_addr = proper_instantiate(&mut app);
-
-            let msg = ExecuteMsg::UpdateOperators {
-                addrs: vec![RANDOM.to_string()],
-            };
-            let err = app
-                .execute_contract(
-                    Addr::unchecked(USER),
-                    permission_module_addr.clone(),
-                    &msg,
-                    &vec![],
-                )
-                .unwrap_err();
-            assert_eq!(
-                err.source().unwrap().to_string(),
-                ContractError::Unauthorized {}.to_string()
-            );
+            assert_eq!(res.data, "contract1")
         }
     }
 }
