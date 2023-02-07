39764f4264bdc18dc1686683a2843067a1ff3cdb
findolor <anakisci@gmail.com>
2023-02-07 14:36:40 +0300

    Add contract state

---
+++ contracts/permissions/custom/src/state.rs
@@ -0,0 +1,16 @@
+use cosmwasm_schema::cw_serde;
+use komple_framework_types::shared::{CONFIG_NAMESPACE, PARENT_ADDR_NAMESPACE};
+
+use cosmwasm_std::Addr;
+use cw_storage_plus::Item;
+
+#[cw_serde]
+pub struct Config {
+    pub admin: Addr,
+}
+pub const CONFIG: Item<Config> = Item::new(CONFIG_NAMESPACE);
+
+pub const PERMISSION_MODULE_ADDR: Item<Addr> = Item::new(PARENT_ADDR_NAMESPACE);
+
+/* TODO: Add custom state variables here */
+/* ... */
