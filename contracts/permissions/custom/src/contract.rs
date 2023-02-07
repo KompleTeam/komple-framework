8f2ce8f450fa087dfe0a492a49ad3c244974a1fb
findolor <anakisci@gmail.com>
2023-02-07 14:38:00 +0300

    Add contract template

---
+++ contracts/permissions/custom/src/contract.rs
@@ -0,0 +1,97 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{
+    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+};
+use cw2::set_contract_version;
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, QueryMsg};
+use crate::state::{Config, CONFIG, PERMISSION_MODULE_ADDR};
+
+use komple_framework_types::shared::query::ResponseWrapper;
+use komple_framework_types::shared::RegisterMsg;
+use komple_framework_utils::response::EventHelper;
+use komple_framework_utils::response::ResponseHelper;
+
+// version info for migration info
+/* TODO: Change contract name here */
+const CONTRACT_NAME: &str = "crates.io:komple-framework-custom-permission";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: RegisterMsg,
+) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    let admin = deps.api.addr_validate(&msg.admin)?;
+    let config = Config {
+        admin: admin.clone(),
+    };
+    CONFIG.save(deps.storage, &config)?;
+
+    PERMISSION_MODULE_ADDR.save(deps.storage, &info.sender)?;
+
+    Ok(
+        /* TODO: Change permission name here */
+        ResponseHelper::new_permission("custom_name", "instantiate").add_event(
+            /* TODO: Change permission name here */
+            EventHelper::new("custom_permission_instantiate")
+                .add_attribute("admin", admin)
+                .add_attribute("permission_module_addr", info.sender)
+                .get(),
+        ),
+    )
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    match msg {
+        ExecuteMsg::Check { data } => execute_check(deps, env, info, data),
+    }
+}
+
+pub fn execute_check(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    data: Binary,
+) -> Result<Response, ContractError> {
+    let permission_addr = PERMISSION_MODULE_ADDR.load(deps.storage)?;
+    
+    /* TODO: Add your logic here */
+    /* ... */
+
+    Ok(
+        /* TODO: Change permission name here */
+        ResponseHelper::new_permission("custom_name", "check").add_event(
+            /* TODO: Change permission name here */
+            EventHelper::new("custom_permission_check")
+                .get(),
+        ),
+    )
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::Config {} => to_binary(&query_config(deps)?),
+    }
+}
+
+fn query_config(deps: Deps) -> StdResult<ResponseWrapper<Config>> {
+    let config = CONFIG.load(deps.storage)?;
+    Ok(ResponseWrapper {
+        query: "config".to_string(),
+        data: config,
+    })
+}
