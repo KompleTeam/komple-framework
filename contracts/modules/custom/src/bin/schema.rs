0867c3ae51c09c284ec5ff71bdec6e9dd6b126b6
findolor <anakisci@gmail.com>
2023-02-07 13:48:32 +0300

    Add contract files

---
+++ contracts/modules/custom/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use komple_framework_custom_module::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
