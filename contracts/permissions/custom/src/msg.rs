43303182d40f3556605cb03c9784122507e8658d
findolor <anakisci@gmail.com>
2023-02-07 14:37:00 +0300

    Add msg template

---
+++ contracts/permissions/custom/src/msg.rs
@@ -0,0 +1,29 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Binary;
+
+use crate::state::Config;
+
+use komple_framework_types::{shared::query::ResponseWrapper, modules::permission::SubPermissionExecuteMsg};
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    Check { data: Binary },
+}
+
+impl From<ExecuteMsg> for SubPermissionExecuteMsg {
+    fn from(msg: ExecuteMsg) -> Self {
+        match msg {
+            ExecuteMsg::Check { data } => SubPermissionExecuteMsg::Check { data },
+        }
+    }
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+    #[returns(ResponseWrapper<Config>)]
+    Config {},
+}
+
+#[cw_serde]
+pub struct MigrateMsg {}
