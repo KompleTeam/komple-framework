f6c222152144eb33860ad3d4f21cc1f40450a777
findolor <anakisci@gmail.com>
2023-02-13 14:30:59 +0300

    Add state

--- contracts/modules/fee/src/state.rs
+++ contracts/modules/fee/src/state.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::cw_serde;
 use komple_framework_types::shared::{
-    CONFIG_NAMESPACE, EXECUTE_LOCK_NAMESPACE, PARENT_ADDR_NAMESPACE,
+    CONFIG_NAMESPACE, EXECUTE_LOCK_NAMESPACE, OPERATORS_NAMESPACE, PARENT_ADDR_NAMESPACE,
 };
 
 use cosmwasm_std::Addr;
@@ -34,3 +34,6 @@ pub const HUB_ADDR: Item<Addr> = Item::new(PARENT_ADDR_NAMESPACE);
 
 /// Lock for the execute entry point.
 pub const EXECUTE_LOCK: Item<bool> = Item::new(EXECUTE_LOCK_NAMESPACE);
+
+/// Operators of this contract.
+pub const OPERATORS: Item<Vec<Addr>> = Item::new(OPERATORS_NAMESPACE);
